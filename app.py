f66035cf804ba6d9edc0edbfe6bcc5e47617155f
Elsa Serrano <elsa2304014@hybridge.education>
2023-07-26 21:02:25 -0600

    correcciones

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, request, redirect
+from entities.student import Student
+from entities.professor import Professor
 
 app = Flask(__name__)
 
-students = []
+students_list = []
+professors_list = []
 
 @app.route("/")
 def index():
     return render_template('index.html', students=students)
 
-@app.route("/")
-def students():
-    return render_template('students.')
 
+@app.route("/students")
+def students():
+    return render_template('students.html', students_list=students_list)
+
+
+@app.route("/professors")
+def professors():
+    return render_template('professors.html', professors_list=professors_list)
 
 @app.route("/new_student", methods=['GET', 'POST'])
 def new_student():
