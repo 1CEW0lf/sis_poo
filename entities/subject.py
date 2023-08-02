65786a0e06db915c9866778cfb3e623276b9cdb6
Elsa Serrano <elsa2304014@hybridge.education>
2023-08-02 13:08:37 -0600

    add subjects and new_subjects, and bootstrap

---
+++ entities/subject.py
@@ -0,0 +1,6 @@
+class Subject:
+    def __init__(self, codigo, nombre, creditos, descripcion):
+        self.codigo_materia = codigo
+        self.nombre_materia = nombre
+        self.creditos_materia = creditos
+        self.descripcion_materia = descripcion
